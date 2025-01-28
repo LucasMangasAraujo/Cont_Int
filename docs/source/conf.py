fbdb62d62bdca967c47f9045050667e911afbf34
LucasMangasAraujo <lucas.mangasaraujo@jesus.ox.ac.uk>
2025-01-28 15:56:08 +0000

    conf.py for read the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ exclude_patterns = []
 
 
 
-# -- Options for HTML output -------------------------------------------------
+# -- Options for HTML output --------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
